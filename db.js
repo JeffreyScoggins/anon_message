81efe8da202be899c526e66b9c446a970fa1748f
Jeffrey Scoggins <JeffreyScoggins84@gmail.com>
2025-02-15 18:07:43 -0500

    added dotenv

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const mysql = require ('mysql2/promise');
+require ('dotenv').config()
 
 const mysqlPool = mysql.createPool({
     host: 'localhost',
-    user: 'root',
-    password: '!mynameJeff444',
-    database: 'posts_db'
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.db_name
 
 });
 
