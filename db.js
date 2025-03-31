cb109f7067583b50dd4713dde4bd5316beed7531
JeffreyScoggins <JeffreyScoggins84@gmail.com>
2025-03-31 08:07:35 -0400

    Update db.js

--- db.js
+++ db.js
@@ -2,10 +2,12 @@ const mysql = require ('mysql2/promise');
 require ('dotenv').config()
 
 const mysqlPool = mysql.createPool({ //connects to the database
-    host: 'anondb.mysql.database.azure.com',
+    host: process.env.db_host,
+    port: process.env.db_port,
     user: process.env.user,
     password: process.env.password, // username and password for the database are stored in the .env file
     database: process.env.db_name
+
 });
 
 mysqlPool.query("SELECT 1")
